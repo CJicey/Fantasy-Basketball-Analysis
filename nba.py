f79bb3e5338c1cbdeb6982399fc9e4f3332b70e0
Caleb Johnson <cjohn734@students.kennesaw.edu>
2025-02-28 13:34:49 -0500

    Update nba.py

--- nba.py
+++ nba.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from nba_api.stats.endpoints import leaguedashplayerstats
 
 fb1 = pd.read_excel(r"C:\Users\leben\Downloads\Fantasy Basketball 2020 - 2024.xlsx", sheet_name="2020 - 2021")
 print(fb1.head())
@@ -15,3 +16,7 @@ print()
 fb4 = pd.read_excel(r"C:\Users\leben\Downloads\Fantasy Basketball 2020 - 2024.xlsx", sheet_name="2023 - 2024")
 print(fb4.head())
 print()
+
+player_stats = leaguedashplayerstats.LeagueDashPlayerStats(season="2024-25").get_data_frames()[0]
+top_50_players = player_stats.sort_values(by="PTS", ascending=False).head(50)
+print(top_50_players[["PLAYER_NAME", "TEAM", "PTS", "REB", "AST", "STL"]])
